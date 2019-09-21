17013787351b47d1dcf26c100f1d4f6b282696b5
Jactsang <jac.tsang.work@gmail.com>
2019-09-21 19:15:44 +0800

    fix bugs

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,5 +1,61 @@
 //animated slideInDown delay-2s
 
+// play audio for year 1992 session
+
+var audio1992 = document.getElementById("audio-1992");
+
+//audio1992.addEventListener("load", playAudio)
+
+function playAudio(){
+    audio1992.play();
+}
+
+function pauseAudio(){
+    audio1992.pause();
+}
+
+var playing1992 = false;
+
+$('#oldTape-1992').hide();
+
+$("#intro-1992").click(function(){
+    
+
+    if(!$('#oldTape-1992').is(':visible')){
+        $('#oldTape-1992 img').addClass('animated slideInDown')
+        $('#oldTape-1992 i').addClass('animated slideInUp')
+    }else{
+        $('#oldTape-1992 img').removeClass('animated slideInDown')
+        $('#oldTape-1992 i').removeClass('animated slideInUp')
+    }
+
+    $('#oldTape-1992').toggle();
+
+    if(playing1992 === true){
+        pauseAudio();
+        return playing1992 = false;
+    }else{
+        playAudio();
+        return playing1992 = true;
+    }
+    
+})
+
+$("#oldTape-1992 i").click(function(){
+    var thisTape = $(this)
+    if(playing1992 === true){
+        thisTape.removeClass("fa-stop");
+        thisTape.addClass("fa-play")
+        pauseAudio();
+        return playing1992 = false;
+    }else{
+        thisTape.removeClass("fa-play");
+        thisTape.addClass("fa-stop");
+        playAudio();
+        return playing1992 = true;
+    }
+})
+
 // play button effect setting
 
 $("#top-description a").mousedown(function () {
